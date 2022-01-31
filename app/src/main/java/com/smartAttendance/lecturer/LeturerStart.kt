f14efe698c2cada8e26bd2e223367572f38ccdad
Dominic Kuncik <itsdomhd@gmail.com>
2022-01-31 20:37:06 +0000

    UI Modification
    Completed all UI updates and renovations

--- app/src/main/java/com/smartAttendance/lecturer/LeturerStart.kt
+++ app/src/main/java/com/smartAttendance/lecturer/LeturerStart.kt
@@ -19,7 +19,7 @@ class LeturerStart : Fragment(), View.OnClickListener {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.lecturer_class_start, container, false)
+        return inflater.inflate(R.layout.lecturer_class_attending, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
