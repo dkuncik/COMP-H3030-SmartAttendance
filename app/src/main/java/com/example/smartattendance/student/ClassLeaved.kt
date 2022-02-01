51e98b0b0e31e49952bc7bdf407e9b89abe24afc
Dominic Kuncik <itsdomhd@gmail.com>
2022-02-01 23:33:04 +0000

    Fixing things
    Fingers crossed?

---
+++ app/src/main/java/com/example/smartattendance/student/ClassLeaved.kt
@@ -0,0 +1,34 @@
+package com.smartAttendance.student
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.smartattendance.R
+
+
+class ClassLeaved : Fragment(), View.OnClickListener {
+    var navController: NavController? = null
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.student_class_left, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+
+    }
+
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
+    }
+
+
+}
