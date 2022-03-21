06d597f239cef0af663054c620ccbc1fdfc940c1
Dominic Kuncik <itsdomhd@gmail.com>
2022-03-21 23:30:27 +0000

    Calendar Attempts

--- app/src/main/java/com/example/smartattendance/student/Student_Main.java
+++ app/src/main/java/com/example/smartattendance/student/Student_Main.java
@@ -1,6 +1,5 @@
 package com.example.smartattendance.student;
 
-<<<<<<< Updated upstream
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Intent;
@@ -9,26 +8,26 @@ import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.provider.CalendarContract;
+import android.util.Log;
 import android.view.View;
-=======
 import android.content.Intent;
 import android.os.Bundle;
->>>>>>> Stashed changes
 import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.smartattendance.R;
+import com.google.api.services.calendar.model.CalendarList;
+import com.google.api.services.calendar.model.CalendarListEntry;
+
+import java.util.List;
 
 public class Student_Main extends AppCompatActivity {
-<<<<<<< Updated upstream
-    private Intent intent;
-    Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-    NfcAdapter nfcAdapter;
-    String classroom = "E043";
-=======
->>>>>>> Stashed changes
+
+    CalendarList calendarList = new CalendarList();
+    List<CalendarListEntry> calendarListEntryList = calendarList.getItems();
 
 
     @Override
@@ -37,42 +36,19 @@ public class Student_Main extends AppCompatActivity {
         setContentView(R.layout.student_main);
 
         Button start = findViewById(R.id.studentMainMenu_ScanTag);
-<<<<<<< Updated upstream
-        Button settings = findViewById(R.id.studentMainMenu_Settings);start.setOnClickListener(view -> {
-=======
         Button settings = findViewById(R.id.studentMainMenu_Settings);
         Button timetable = findViewById(R.id.studentMainMenu_Timetable);
-        start.setOnClickListener(view -> {
->>>>>>> Stashed changes
-            // Mihai add the NFC things here to ask for scanning
-            onNewIntent(intent);PendingIntent pendingIntent = PendingIntent.getActivity(
-                   this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+
+        timetable.setOnClickListener(view -> {
+            Log.i("TAG", String.valueOf(calendarList.getItems()));
         });
-        settings.setOnClickListener(view -> openActivity(Student_Settings.class));
-//        timetable.setOnClickListener(view -> openActivity(Student_Timetable.class));
     }
+
     public void openActivity(Class activity) {
         Intent intent = new Intent(this, activity);
         startActivity(intent);
     }
-<<<<<<< Updated upstream
-    @Override
-    protected void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-
-        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
-            Parcelable[] rawMessages =
-                    intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
-            if (rawMessages != null) {
-                NdefMessage[] messages = new NdefMessage[rawMessages.length];
-                for (int i = 0; i < rawMessages.length; i++) {
-                    messages[i] = (NdefMessage) rawMessages[i];
-                }
-                // Process the messages array.
-            }
-        }
-    }
-
-=======
->>>>>>> Stashed changes
 }
+
+
+
